3b262d6a63019522d13971cd8743767d7be5d557
AlexParreira <alexander.o.parreira@gmail.com>
2023-08-16 22:21:00 -0300

    Initial Commit

---
+++ SheetRPG/SheetRPGApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetRPGApp.swift
+//  SheetRPG
+//
+//  Created by Alexander Parreira on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SheetRPGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
